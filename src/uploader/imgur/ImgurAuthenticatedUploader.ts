4ed7c4bf93ce0996888dff357a0d67b75701e8d7
Hai Nguyen <hai@ahijsc.com>
2023-02-13 11:05:40 +0700

    draft image logging

--- src/uploader/imgur/ImgurAuthenticatedUploader.ts
+++ src/uploader/imgur/ImgurAuthenticatedUploader.ts
@@ -1,6 +1,18 @@
 import ImgurClient from "../../imgur/ImgurClient";
 import ImageUploader from "../ImageUploader";
 
+function logImage(image: string) {
+  fs.appendFile("ImgurUploadedImages.log", `${image}\n`, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+//const $image = 'https://i.imgur.com/abc123.jpg';
+
+logImage($image);
+
 export default class ImgurAuthenticatedUploader implements ImageUploader {
   constructor(readonly client: ImgurClient) {}
 
@@ -8,8 +20,10 @@ export default class ImgurAuthenticatedUploader implements ImageUploader {
     const imgurImageLink = (await this.client.upload(image)).data.link;
     // return (await this.client.upload(image)).data.link;
     // console.log("imgurImageLink: ", imgurImageLink);
-    console.log("image: ", image);
-    const localImageLink = "localImageLink";
-    return `${imgurImageLink}?${localImageLink}`;
+    // console.log("image: ", image);
+    const localImageLink = image.name;
+    const returnImageLink = `${imgurImageLink}?${localImageLink}`;
+    console.log("returnImageLink: ", returnImageLink);
+    return returnImageLink;
   }
 }
